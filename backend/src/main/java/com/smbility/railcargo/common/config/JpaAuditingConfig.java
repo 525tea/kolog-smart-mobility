1bb2ade8f105615de908a0b471ff442f999f5064
Lee Ye Jin <jene0028@gmail.com>
2026-08-13 10:06:21 +0900

    feat(common): configure JPA auditing

---
+++ backend/src/main/java/com/smbility/railcargo/common/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.smbility.railcargo.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
